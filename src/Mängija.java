2eaaba788d6911ff009c29a754bc3deacb993256
karlerikott <karlerik.ott@gmail.com>
2021-04-07 14:48:34 +0300

    Peaklassis andmete sisselugemine, koostab mängijate listi Mängijad klassis setVanus ja setJooks konstruktoris

--- src/Mängija.java
+++ src/Mängija.java
@@ -4,43 +4,37 @@ public class Mängija {
     private String nimi;
     private int väravad;
     private double jooks;
-    private String positsioon;
     private int särk;
     private String klubi;
-    private int sünniaasta;
     private int vanus;
 
     public Mängija(String nimi,int sünniaasta, String positsioon, String klubi, int väravad, int särk) {
         this.nimi = nimi;
         this.väravad = väravad;
-        this.positsioon = positsioon;
         this.särk = särk;
         this.klubi = klubi;
+        this.vanus = setVanus(sünniaasta);
+        this.jooks = setJooks(positsioon);
     }
 
-    public void setJooks(String positsioon){
+    public double setJooks(String positsioon){
         switch(positsioon){
             case "ründaja":
-                jooks = 7.0+(Math.random()*2.0); //Keskmine distants 7km - 9km
-                break;
+                return 7.0+(Math.random()*2.0); //Keskmine distants 7km - 9km
             case "poolkaitsja":
-                jooks = 8.0+(Math.random()*4.0); //Keskmine distants 8km - 12km
-                break;
+                return 8.0+(Math.random()*4.0); //Keskmine distants 8km - 12km
             case "kaitsja":
-                jooks = 5.0+(Math.random()*3.0);//Keskmine distants 5km - 8km
-                break;
+                return 5.0+(Math.random()*3.0);//Keskmine distants 5km - 8km
             case "väravavaht":
-                jooks = 1.0+(Math.random());//Keskmine distants 1km - 2km
-                break;
+                return 1.0+(Math.random());//Keskmine distants 1km - 2km
             default:
-                jooks = 6.0+(Math.random()*4.0); //Keskmine distants 6km - 10km
-                break;
+                return 6.0+(Math.random()*4.0); //Keskmine distants 6km - 10km
         }
     }
 
-    public void setVanus(int sünniaasta){
+    public int setVanus(int sünniaasta){
         int aasta = Year.now().getValue();
-        vanus = aasta-sünniaasta; //Leiab isiku vanuse
+        return aasta-sünniaasta; //Leiab isiku vanuse
     }
 
 
--- src/Peaklass.java
+++ src/Peaklass.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Peaklass {
     // Küsib kasutajalt mida teha? (Vaata mängijate infot/ vaata liiga infot)
     // Mängijate info valiku korral annab valiku klubidest ja seejärel mängijatest ning kuvab vastava info
@@ -9,6 +14,21 @@ public class Peaklass {
     // Klubide isendid (sisaldab endas listi mängijate isenditest, kes seal klubis mängivad)
     // Liiga isend (sisaldab listi klubide isenditest)
 
+    public static List<Mängija> loeFail(String failinimi) throws Exception{ //Loeb failist mängijate andmed
+        List<Mängija> mängijad = new ArrayList<>();
+        File objekt = new File(failinimi);
+        Scanner lugeja = new Scanner(objekt);
+        while(lugeja.hasNextLine()){
+            String rida = lugeja.nextLine();
+            String[] andmed = rida.split(", ");
+            Mängija mängija = new Mängija(andmed[0],Integer.parseInt(andmed[1]),andmed[2],andmed[3],Integer.parseInt(andmed[4]),Integer.parseInt(andmed[5])); //Loob uue mängija
+            mängijad.add(mängija); //Lisab mängija mängijate järjendisse
+        }
+        return mängijad;
+    }
+    //Mängijate järjendi põhjal saab luua klubid
+
     public static void main(String[] args) {
+
     }
 }
